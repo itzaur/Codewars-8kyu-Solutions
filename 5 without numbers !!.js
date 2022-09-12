4d9d64b636bbc0d313ea6ce2ce3d3c510235e7e3
itzaur <preneur85@gmail.com>
2022-09-12 23:27:13 +0300

    Add 5 without numbers !!

---
+++ 5 without numbers !!.js
@@ -0,0 +1,17 @@
+/*
+Description:
+
+Write a function that always returns 5
+
+Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+Good luck :)
+*/
+function unusualFive() {
+  return "five!".length;
+  //   return new Date().getMonth() - 3;
+  //   return ((true << true) << true) | true;
+}
+
+console.log(unusualFive());
+console.log(unusualFive("gh"));
