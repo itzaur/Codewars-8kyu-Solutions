6fb4f73132e0b5dc689ee7abcffd3c9a8f026b9e
itzaur <preneur85@gmail.com>
2022-10-12 22:33:15 +0300

    Add All Star Code Challenge #18

---
+++ All Star Code Challenge #18.js
@@ -0,0 +1,47 @@
+/*
+Description:
+
+This Kata is intended as a small challenge for my students
+
+All Star Code Challenge #18
+
+Create a function that accepts 2 string arguments and returns an integer of the count of occurrences the 2nd argument is found in the first one.
+
+If no occurrences can be found, a count of 0 should be returned.
+
+("Hello", "o")  ==>  1
+("Hello", "l")  ==>  2
+("", "z")       ==>  0
+
+Notes:
+
+    The first argument can be an empty string
+    The second string argument will always be of length 1
+
+*/
+function strCount(str, letter) {
+  return [...str].filter((el) => el === letter).length;
+  // return str.split(letter).length - 1
+
+  //   let count = 0;
+  //   [...str].forEach((el) => (el === letter ? count++ : count));
+  //   return count;
+
+  //   return [...str].reduce((acc, el) => {
+  //     // return acc + letter.includes(el)
+  //     // return el === letter ? acc + 1 : acc
+  //     return acc + (el === letter);
+  //   }, 0);
+
+  //   return [...str].map((el) => el === letter).filter(Boolean).length;
+
+  //   let obj = {};
+  //   for (let val of str) {
+  //     obj[val] = ++obj[val] || 1;
+  //   }
+  //   return obj[letter] || 0;
+}
+
+console.log(strCount("Hello", "o")); //1
+console.log(strCount("Hello", "l")); //2
+console.log(strCount("", "z")); //0
