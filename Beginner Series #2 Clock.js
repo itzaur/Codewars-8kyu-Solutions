96415e5946679cd456744cdee875edc657c80642
itzaur <preneur85@gmail.com>
2022-11-02 21:18:11 +0300

    Add Beginner Series #2 Clock

---
+++ Beginner Series #2 Clock.js
@@ -0,0 +1,37 @@
+/*
+Description:
+
+Clock shows h hours, m minutes and s seconds after midnight.
+
+Your task is to write a function which returns the time since midnight in milliseconds.
+Example:
+
+h = 0
+m = 1
+s = 1
+
+result = 61000
+
+Input constraints:
+
+    0 <= h <= 23
+    0 <= m <= 59
+    0 <= s <= 59
+*/
+function past(h, m, s) {
+  return (h * 60 * 60 + m * 60 + s) * 1000;
+  //   return new Date(0, 0, 0, h, m, s) - new Date(0, 0, 0);
+  //   return new Date().setHours(h, m, s) - new Date().setHours(0, 0, 0);
+}
+
+// function past(...args) {
+//   return args.reduce((acc, el) => {
+//     return acc * 60 + el;
+//   }, 0) * 1e3;
+// }
+
+console.log(past(0, 1, 1)); //61000
+console.log(past(1, 1, 1)); //3661000
+console.log(past(0, 0, 0)); //0
+console.log(past(1, 0, 1)); //3601000
+console.log(past(1, 0, 0)); //3600000
