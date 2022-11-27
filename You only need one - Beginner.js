3486f5671cd5abe424c167c9ca90fcd2be6ee825
itzaur <preneur85@gmail.com>
2022-11-27 12:03:14 +0300

    Add You only need one - Beginner

---
+++ You only need one - Beginner.js
@@ -0,0 +1,20 @@
+/*
+Description:
+
+You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+Array can contain numbers or strings. X can be either.
+
+Return true if the array contains the value, false if not.
+*/
+function check(a, x) {
+  return a.includes(x);
+  // return a.indexOf(x) > - 1
+  // return a.some((el) => el === x);
+  // return new Set(a).has(x)
+}
+
+console.log(check([66, 101], 66)); //true
+console.log(check([101, 45, 75, 105, 99, 107], 107)); //true
+console.log(check(["t", "e", "s", "t"], "e")); //true
+console.log(check(["what", "a", "great", "kata"], "kat")); //false
