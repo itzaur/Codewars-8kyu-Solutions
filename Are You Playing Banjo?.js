df7684fb6ba2536fe30cdab32e8c19ad89fd824a
itzaur <preneur85@gmail.com>
2022-12-06 10:49:10 +0300

    Add Are You Playing Banjo?

---
+++ Are You Playing Banjo?.js
@@ -0,0 +1,36 @@
+/*
+Description:
+
+Create a function which answers the question "Are you playing banjo?".
+If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+The function takes a name as its only argument, and returns one of the following strings:
+
+name + " plays banjo" 
+name + " does not play banjo"
+
+Names given are always valid strings.
+*/
+function areYouPlayingBanjo(name) {
+  return name.slice(0, 1).toLowerCase() === "r"
+    ? `${name} plays banjo`
+    : `${name} does not play banjo`;
+
+  // return name.toLowerCase().startsWith("r") ? `${name} plays banjo` : `${name} does not play banjo`;
+
+  // return name + (/^r/i.test(name) ? " plays" : " does not play") + " banjo"
+
+  // return name.match(/^r/i) ? `${name} plays banjo` : `${name} does not play banjo`;
+
+  //   return name.at().toLowerCase() === "r"
+  //     ? `${name} plays banjo`
+  //     : `${name} does not play banjo`;
+}
+
+console.log(areYouPlayingBanjo("Adam")); //"Adam does not play banjo"
+console.log(areYouPlayingBanjo("Paul")); //"Paul does not play banjo"
+console.log(areYouPlayingBanjo("Ringo")); //"Ringo plays banjo"
+console.log(areYouPlayingBanjo("bravo")); //"bravo does not play banjo"
+console.log(areYouPlayingBanjo("rolf")); //"rolf plays banjo"
+
+console.log("klkk".startsWith("k"));
