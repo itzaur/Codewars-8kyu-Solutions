1be9e1976201e8e03fd46fa0e03c4f5d70e7c59a
itzaur <preneur85@gmail.com>
2022-12-14 13:26:19 +0300

    Add Century From Year

---
+++ Century From Year.js
@@ -0,0 +1,42 @@
+/*
+Description:
+Introduction
+
+The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+Task
+
+Given a year, return the century it is in.
+Examples
+
+1705 --> 18
+1900 --> 19
+1601 --> 17
+2000 --> 20
+*/
+function century(year) {
+  return Math.ceil(year / 100);
+
+  //   let century = 0;
+  //   for (let i = 0; i < year; i++) {
+  //     if (i % 100 === 0) {
+  //       century++;
+  //     }
+  //   }
+
+  //   return century;
+
+  //   return (--year / 100 + 1) | 0;
+
+  //   let century = 1;
+  //   while (year > 100) {
+  //     year -= 100;
+  //     century++;
+  //   }
+  //   return century;
+}
+
+console.log(century(1705)); //18
+console.log(century(1900)); //19
+console.log(century(1601)); //17
+console.log(century(2000)); //20
+console.log(century(89)); //1
