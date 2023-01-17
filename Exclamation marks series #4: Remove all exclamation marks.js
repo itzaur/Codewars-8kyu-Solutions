d38981a9fa48cbf14e368aa9d451172c634446cb
itzaur <preneur85@gmail.com>
2023-01-17 09:10:58 +0300

    Add Exclamation marks series #4

---
+++ Exclamation marks series #4: Remove all exclamation marks.js
@@ -0,0 +1,26 @@
+/*
+Description:
+Description:
+
+Remove all exclamation marks from sentence but ensure a exclamation mark at the end of string. For a beginner kata, you can assume that the input data is always a non empty string, no need to verify it.
+Examples
+
+remove("Hi!") === "Hi!"
+remove("Hi!!!") === "Hi!"
+remove("!Hi") === "Hi!"
+remove("!Hi!") === "Hi!"
+remove("Hi! Hi!") === "Hi Hi!"
+remove("Hi") === "Hi!"
+*/
+function remove(string) {
+  return string.split`!`.join`` + "!";
+
+  // return `${string.replace(/!/g, "")}!`
+}
+
+console.log(remove("Hi!")); //"Hi!"
+console.log(remove("Hi!!!")); //"Hi!"
+console.log(remove("!Hi")); //"Hi!"
+console.log(remove("!Hi!")); //"Hi!"
+console.log(remove("Hi! Hi!")); //"Hi Hi!"
+console.log(remove("Hi")); //"Hi!"
