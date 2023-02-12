610b68da8e91cd804eb9bd71a32822047d105dc9
itzaur <preneur85@gmail.com>
2023-02-12 10:58:52 +0300

    Add Grasshopper - Variable Assignment Debug

---
+++ Grasshopper - Variable Assignment Debug.js
@@ -0,0 +1,13 @@
+/*
+Description:
+Variable assignment
+
+Fix the variables assigments so that this code stores the string 'devLab' in the variable name.
+*/
+let a = "dev",
+  b = "Lab";
+let name = a + b;
+
+console.log(a); //"dev"
+console.log(b); //"Lab"
+console.log(name); //"devLab"
