b0f2ee78dad9f54e2f3480924eb9ff32848a354c
itzaur <preneur85@gmail.com>
2023-05-04 23:33:35 +0300

    Add Color Ghost

---
+++ Color Ghost.js
@@ -0,0 +1,22 @@
+/*
+DESCRIPTION:
+Color Ghost
+Create a class Ghost
+
+Ghost objects are instantiated without any arguments.
+
+Ghost objects are given a random color attribute of "white" or "yellow" or "purple" or "red" when instantiated
+
+ghost = new Ghost();
+ghost.color //=> "white" or "yellow" or "purple" or "red"
+*/
+class Ghost {
+  constructor() {
+    const colors = ['white', 'yellow', 'purple', 'red'];
+    const length = colors.length;
+    this.color = colors[(Math.random() * length).toFixed(0)];
+  }
+}
+
+const ghost = new Ghost();
+console.log(ghost.color);
