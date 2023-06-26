9b05c05bbf50cf8f0f4972390e7bc78564b76e7a
itzaur <preneur85@gmail.com>
2023-06-26 22:25:44 +0300

    Add simple calculator

---
+++ simple calculator.js
@@ -0,0 +1,37 @@
+/*
+Description:
+
+You are required to create a simple calculator that returns the result of addition, subtraction, multiplication or division of two numbers.
+
+Your function will accept three arguments:
+The first and second argument should be numbers.
+The third argument should represent a sign indicating the operation to perform on these two numbers.
+
+if the variables are not numbers or the sign does not belong to the list above a message "unknown value" must be returned.
+Example:
+
+calculator(1,2,"+"); //=> result will be 3
+calculator(1,2,"&"); //=> result will be "unknown value"
+calculator(1,"k","*"); //=> result will be "unknown value"
+
+Good luck!
+*/
+function calculator(a, b, sign) {
+  const operations = {
+    '+': a + b,
+    '-': a - b,
+    '*': a * b,
+    '/': a / b,
+  };
+
+  if (typeof a !== 'number' || typeof b !== 'number') return 'unknown value';
+
+  return operations[sign] || 'unknown value';
+}
+
+console.log(calculator(1, 2, '+')); //3
+console.log(calculator(1, 2, '-')); //-1
+console.log(calculator(3, 5, '*')); //15
+console.log(calculator(6, 2, '/')); //3
+console.log(calculator(6, 2, '$')); //'unknown value'
+console.log(calculator(6, 'h', '*')); //'unknown value'
