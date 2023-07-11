5cd1acfb740462454f7fb8ff7418c4946f949079
itzaur <preneur85@gmail.com>
2023-07-11 10:58:04 +0300

    Add Find the Difference in Age between Oldest and Youngest Family Members

---
+++ Find the Difference in Age between Oldest and Youngest Family Members.js
@@ -0,0 +1,18 @@
+/*
+Description:
+
+At the annual family gathering, the family likes to find the oldest living family member’s age and the youngest family member’s age and calculate the difference between them.
+
+You will be given an array of all the family members' ages, in any order. The ages will be given in whole numbers, so a baby of 5 months, will have an ascribed ‘age’ of 0. Return a new array (a tuple in Python) with [youngest age, oldest age, difference between the youngest and oldest age].
+*/
+function differenceInAges(ages) {
+    const maxAge = Math.floor(Math.max(...ages));
+    const minAge = Math.floor(Math.min(...ages));
+    const difference = maxAge - minAge;
+    
+    return [minAge, maxAge, difference]
+}
+
+console.log(differenceInAges([82, 15, 6, 38, 35])); //[6, 82, 76]
+console.log(differenceInAges([57, 99, 14, 32])); //[14, 99, 85]
+console.log(differenceInAges([57, 0.5, 14, 32])); //[14, 99, 85]
