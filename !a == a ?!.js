4c93f74e0b3357acfdfc50622e9265deefa5a07d
itzaur <preneur85@gmail.com>
2024-12-18 13:20:21 +0300

    Add !a == a ?!

---
+++ !a == a ?!.js
@@ -0,0 +1,25 @@
+/*
+Description:
+You task to pass only this tests :
+
+a == false
+!a == false
+a == !a
+*/
+const a = [];
+////////////////////!SECTION
+// const a = '0';
+////////////////////!SECTION
+// const a = new Boolean();
+////////////////////!SECTION
+// const a = ' ';
+////////////////////!SECTION
+// const a = Array();
+////////////////////!SECTION
+// const a = new Number;
+////////////////////!SECTION
+// const a = new String;
+
+console.log(a == false); //true
+console.log(!a == false); //true
+console.log(a == !a); //true
